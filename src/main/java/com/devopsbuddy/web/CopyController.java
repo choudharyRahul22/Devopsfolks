7a1afe8a7724667cc0220c67fbb4bb2ad6cf8cc3
choudharyRahul22 <choudharyRahul22@outlook.com>
2018-09-24 21:51:24 +0530

    logging

---
+++ src/main/java/com/devopsbuddy/web/CopyController.java
@@ -0,0 +1,13 @@
+package com.devopsbuddy.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class CopyController {
+
+    @RequestMapping("/about")
+    public String aboutPage() {
+        return "copy/about";
+    }
+}
