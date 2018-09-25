0184d346cf6701017c8d90664c47865974a3e555
choudharyRahul22 <choudharyRahul22@outlook.com>
2018-09-25 07:47:43 +0530

    about

--- src/main/java/com/devopsbuddy/web/controller/CopyController.java
+++ src/main/java/com/devopsbuddy/web/controller/CopyController.java
@@ -1,4 +1,4 @@
-package com.devopsbuddy.web;
+package com.devopsbuddy.web.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
